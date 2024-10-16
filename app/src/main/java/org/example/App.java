ff3cbc84176fc71fce3791cd3a4fa90091581896
PizzaPapst <maik.bartels2@gmail.com>
2024-10-16 19:42:16 +0200

    test

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,6 +10,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-	System.out.println("Hello World!");
+	System.out.println("Hello Worldsdsd!");
     }
 }
